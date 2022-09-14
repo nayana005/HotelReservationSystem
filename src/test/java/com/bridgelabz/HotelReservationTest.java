a43369914ece2795b9ac44b5c8ad16aab7d9784d
nayana005 <nayanasuresh1005@gmail.com>
2022-09-14 19:33:50 +0530

    Added: Best Rated Hotel

--- src/test/java/com/bridgelabz/HotelReservationTest.java
+++ src/test/java/com/bridgelabz/HotelReservationTest.java
@@ -9,28 +9,28 @@ public class HotelReservationTest {
 	@Before
 	public void initialize() {
 		hotelReservation = new HotelReservation();
-		hotelReservation.addHotel(new Hotel("Lakewood", 110, 90));
-		hotelReservation.addHotel(new Hotel("Bridgewood", 160, 50));
-		hotelReservation.addHotel(new Hotel("Ridgewood", 220, 150));
+		hotelReservation.addHotel(new Hotel("Lakewood", 110, 90, 3));
+		hotelReservation.addHotel(new Hotel("Bridgewood", 160, 50, 4));
+		hotelReservation.addHotel(new Hotel("Ridgewood", 220, 150, 5));
 	}
 
 	@Test
 	public void addHotel() {
-		boolean result = hotelReservation.addHotel(new Hotel("Lakewood", 110, 90));
+		boolean result = hotelReservation.addHotel(new Hotel("Lakewood", 110, 90, 3));
 		Assert.assertTrue(result);
 	}
 
 	@Test
-	public void getCheapestHotel() {
-		Hotel hotel = hotelReservation.getCheapestHotel("23-05-2022", "31-05-2022");
-		Assert.assertEquals("Lakewood", hotel.getName());
-	}
-
-	@Test
 	public void addWeekend_weekday() {
-		Hotel hotel = hotelReservation.getCheapestHotel("28-05-2022", "29-05-2022");
+		Hotel hotel = hotelReservation.getCheapestBestRatedHotel("28-05-2022", "29-05-2022");
 		Assert.assertEquals("Bridgewood", hotel.getName());
 	} 
+
+	@Test
+	public void customerShould_getBestRatedHotel() {
+		Hotel hotel = hotelReservation.getBestRatedHotel();
+		Assert.assertEquals("Ridgewood", hotel.getName());
+	}
 
 }
 
